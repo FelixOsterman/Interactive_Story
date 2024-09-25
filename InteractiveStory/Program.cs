55c10efd9d2306be2e8539ce9e68bfa64e7698c6
FelixOsterman <felix.osterman@elev.ga.ntig.se>
2024-09-25 22:46:38 +0200

    klart

---
+++ InteractiveStory/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.WriteLine("Where are you from? ");
+string name = Console.ReadLine();
+
+if (name == "Sweden")
+{
+    Console.WriteLine("Good");
+}
+else if (name == "Norge" || name == "Finland")
+{
+    Console.WriteLine("Okay");
+
+    Console.WriteLine("What do you feel about Sweden? ");
+    string name2 = Console.ReadLine();
+
+    if (name2 == "Like")
+    {
+        Console.WriteLine("Good");
+    }
+    else if (name2 == "Bad")
+    {
+        Console.WriteLine("Die");
+    }
+    else
+    {
+        Console.WriteLine("Uuh");
+    }
+}
+else
+{
+    Console.WriteLine("...");
+
+    Console.WriteLine("Are you shore? ");
+    string name3 = Console.ReadLine();
+
+    if (name3 == "Yes")
+    {
+        Console.WriteLine("Okay");
+    }
+    else if (name3 == "No")
+    {
+        Console.WriteLine("You can retake the questiens");
+    }
+    else
+    {
+        Console.WriteLine("Uuh");
+    }
+}
+
+Console.ReadLine();
